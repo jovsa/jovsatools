713260b5b19f98c6a4614da32618395ab3829487
Jovan Sardinha <jovan.sardinha@gmail.com>
2020-03-22 14:59:15 -0700

    removed core

--- jsmltools/_nbdev.py
+++ jsmltools/_nbdev.py
@@ -4,7 +4,7 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"say_hello": "00_core.ipynb"}
 
-modules = ["core.py"]
+modules = ["hello_nbdev.py"]
 
 doc_url = "https://jovsa.github.io/jsmltools/"
 

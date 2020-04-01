15c20a7d3c47e07e21fda0df10956673b88ab2af
Jovan Sardinha <1289023+jovsa@users.noreply.github.com>
2020-04-01 06:42:17 -0700

    Autoregressive Models from CS294-158 Base Example (#6)
    * initial commit from collab
    
    * udpated settings with ipdb and added __init__
    
    * updated refrences
    
    * added custom pytorch training
    
    * updates to custom pytorch training

--- jsmltools/_nbdev.py
+++ jsmltools/_nbdev.py
@@ -2,9 +2,16 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"generate_1d_data": "custom_pytorch_training.ipynb",
+         "generate_2d_data": "custom_pytorch_training.ipynb",
+         "train": "custom_pytorch_training.ipynb",
+         "eval_loss": "custom_pytorch_training.ipynb",
+         "train_epochs": "custom_pytorch_training.ipynb",
+         "Histogram": "custom_pytorch_training.ipynb",
+         "MixtureOfLogistics": "custom_pytorch_training.ipynb"}
 
-modules = []
+modules = ["custom_pytorch_training.py",
+           "custom_tf_training.py"]
 
 doc_url = "https://jovsa.github.io/jsmltools/"
 

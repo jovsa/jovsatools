aa8db194ffd5269e86962ef57e5f0cebaa6f1176
Jovan Sardinha <1289023+jovsa@users.noreply.github.com>
2020-06-05 00:46:47 -0700

    2 target pipeline (#22)
    * updates to settings.ini
    
    * added data generation notebook
    
    * added fuction approximation notebook
    
    * changed a few nits in setitngs.ini
    
    * updated docs
    
    * updates to library
    
    * updates to notebooks
    
    * updates to README

--- jovsatools/data_generator.py
+++ jovsatools/data_generator.py
@@ -21,7 +21,7 @@ class MNISTDataGenerator(object):
             additional_y: Integer, used to specify the additional targets
             seed = Integer, used to specify np.random.seed()
 
-        Retruns:
+        Returns:
             train_x: numpy.ndarray, MNIST train data flattened
             train_y: numpy.ndarray, all the targets; MNIST label + additional_y
             test_x: numpy.ndarray, MNIST test data flattened
@@ -46,49 +46,46 @@ class MNISTDataGenerator(object):
         self.test_n = len(self.mnist_test_x)
 
     def _initialize(self, additional_y):
-        """prepare functions to approximate """
+        """Prepare functions to approximate """
         epsilon = 0.000123
         C = 102 # emperical value from analyzing MNIST data
 
+        # label 0: mnist class
         if additional_y >= 1:
-            # classification (0, 1) target
+            # label 1: classification (0, 1) target
             self._func_map[0] = lambda x: int(np.random.random()<0.9) if 2*(x[10]**3)-2*x[3]+15 > 8.9 else 0
 
         if additional_y >= 2:
-            # unbounded regression target
-            self._func_map[1] = lambda x: np.log(np.sum(x, axis=0)+epsilon)
+            # label 2: unbounded regression target
+            self._func_map[1] = lambda x: np.log(np.sum(x)+epsilon)
 
         if additional_y >= 3:
-            # bounded (0, 1) regression target
-            self._func_map[2] = lambda x: np.mean(x, axis=0)/C
+            # label 3: bounded (0, 1) regression target
+            self._func_map[2] = lambda x: np.mean(x)/C
 
         # regular MNIST train/test data
         train, test = tf.keras.datasets.mnist.load_data()
         self.mnist_train_x, self.mnist_train_y = train[0], train[1]
         self.mnist_test_x, self.mnist_test_y = test[0], test[1]
 
-    def __call__(self, train_n):
-        """data generation """
-        assert train_n <= self.train_n
+    def prepare_datasets(self, mnist_x, mnist_y, n):
+        """Main worker function for dataset preparation """
+        x, y = [], []
+        for i in range(n):
+            features = mnist_x[i].flatten().reshape(-1, 1)
+            labels = [mnist_y[i]]
+            for f in range(self.additional_y):
+                labels.append(self._func_map[f](features))
+            x.append(features)
+            y.append(labels)
+        return np.asarray(x), np.asarray(y)
+
+    def __call__(self, sample_n):
+        """Data generation call """
+        assert sample_n <= self.train_n, f"max alloable size is {self.train_n}"
         train_x, train_y = [], []
         test_x, test_y = [], []
 
-        #TODO (jovsa): consolidate the two identical loop
-        for i in range(train_n):
-            x = self.mnist_train_x[i].flatten().reshape(-1, 1)
-            y = [self.mnist_train_y[i]]
-            for f in range(self.additional_y):
-                y.append(self._func_map[f](x))
-            train_x.append(x)
-            train_y.append(y)
-
-        # prepating test data
-        for i in range(self.test_n):
-            x = self.mnist_test_x[i].flatten().reshape(-1, 1)
-            y = [self.mnist_test_y[i]]
-            for f in range(self.additional_y):
-                y.append(self._func_map[f](x))
-            test_x.append(x)
-            test_y.append(y)
-
-        return (np.asarray(train_x), np.asarray(train_y), np.asarray(test_x), np.asarray(test_y))
+        train_x, train_y = self.prepare_datasets(self.mnist_train_x, self.mnist_train_y, n=sample_n)
+        test_x, test_y = self.prepare_datasets(self.mnist_test_x, self.mnist_test_y, n=self.test_n)
+        return (train_x, train_y, test_x, test_y)

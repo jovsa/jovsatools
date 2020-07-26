319708f1947c3207ef99dd67771df5b5e1f7792a
Jovan Sardinha <jovan.sardinha@gmail.com>
2020-07-25 23:15:21 -0700

    removed duplicate gdrive

--- jovsatools/_nbdev.py
+++ jovsatools/_nbdev.py
@@ -13,18 +13,18 @@ index = {"generate_1d_data": "custom_pytorch_training.ipynb",
          "KerasPipeline": "function_approximation.ipynb",
          "HighLevelKerasPipeline": "function_approximation.ipynb",
          "HighLevelKerasPipelineMultiTarget": "function_approximation.ipynb",
-         "truncated": "google_drive_activity_tracking.ipynb",
-         "getOneOf": "google_drive_activity_tracking.ipynb",
-         "getTimeInfo": "google_drive_activity_tracking.ipynb",
-         "getActionInfo": "google_drive_activity_tracking.ipynb",
-         "getUserInfo": "google_drive_activity_tracking.ipynb",
-         "getActorInfo": "google_drive_activity_tracking.ipynb",
-         "getTargetInfo": "google_drive_activity_tracking.ipynb",
-         "initialize_data": "google_drive_activity_tracking.ipynb",
+         "truncated": "gdrive_activity_tracking.ipynb",
+         "getOneOf": "gdrive_activity_tracking.ipynb",
+         "getTimeInfo": "gdrive_activity_tracking.ipynb",
+         "getActionInfo": "gdrive_activity_tracking.ipynb",
+         "getUserInfo": "gdrive_activity_tracking.ipynb",
+         "getActorInfo": "gdrive_activity_tracking.ipynb",
+         "getTargetInfo": "gdrive_activity_tracking.ipynb",
+         "initialize_data": "gdrive_activity_tracking.ipynb",
          "get_authors": "gdrive_activity_tracking.ipynb",
          "convert_dates": "gdrive_activity_tracking.ipynb",
-         "get_drive_activity": "google_drive_activity_tracking.ipynb",
-         "SCOPES": "google_drive_activity_tracking.ipynb",
+         "get_drive_activity": "gdrive_activity_tracking.ipynb",
+         "SCOPES": "gdrive_activity_tracking.ipynb",
          "clean_data": "gdrive_activity_tracking.ipynb",
          "get_data": "gdrive_activity_tracking.ipynb"}
 
@@ -34,8 +34,7 @@ modules = ["custom_pytorch_training.py",
            "data_generator.py",
            "dice_experiments.py",
            "function_approximation.py",
-           "gdrive_activity_tracking.py",
-           "google_drive_activity_tracking.py"]
+           "gdrive_activity_tracking.py"]
 
 doc_url = "https://jovsa.github.io/jovsatools/"
 

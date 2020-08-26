8e90efc2fdbeb6b04e3b927756767a93bdaef642
Jovan Sardinha <jovan.sardinha@gmail.com>
2020-08-25 19:41:08 -0700

    compleated task 0.1

--- jovsatools/minitorch/Module-0/minitorch/operators.py
+++ jovsatools/minitorch/Module-0/minitorch/operators.py
@@ -6,44 +6,40 @@ import math
 
 def mul(x, y):
     ":math:`f(x, y) = x * y`"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    return x * y
 
 
 def id(x):
     ":math:`f(x) = x`"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    return x
 
 
 def add(x, y):
     ":math:`f(x, y) = x + y`"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    return x+y
+
 
 
 def neg(x):
     ":math:`f(x) = -x`"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    return -1*x
 
 
 def lt(x, y):
     ":math:`f(x) =` 1.0 if x is greater than y else 0.0"
     # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    return 1.0 if x > y else 0.0
 
 
 def eq(x, y):
     ":math:`f(x) =` 1.0 if x is equal to y else 0.0"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    return 1.0 if x==y else 0.0
+
 
 
 def max(x, y):
     ":math:`f(x) =` x if x is greater than y else y"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    return x if x>y else y
 
 
 def sigmoid(x):
@@ -59,8 +55,10 @@ def sigmoid(x):
     for stability.
 
     """
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    if x >= 0.0:
+        return 1.0/(1.0+math.exp(-x))
+    else:
+        return (math.exp(x))/(1.0+math.exp(x))
 
 
 def relu(x):
@@ -69,14 +67,12 @@ def relu(x):
 
     (See `<https://en.wikipedia.org/wiki/Rectifier_(neural_networks)>`_ .)
     """
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    return x if x > 0.0 else 0.0
 
 
 def relu_back(x, y):
     ":math:`f(x) =` y if x is greater than 0 else 0"
-    # TODO: Implement for Task 0.1.
-    raise NotImplementedError('Need to implement for Task 0.1')
+    return y if x > 0.0 else 0.0
 
 
 EPS = 1e-6

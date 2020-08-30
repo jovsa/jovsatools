c9e1cc739b3cb8e16e2132ddd116c7f426df1dae
Jovan Sardinha <jovan.sardinha@gmail.com>
2020-08-30 11:19:57 -0700

    imported changes from Module-0

--- jovsatools/minitorch/Module-1/minitorch/modules.py
+++ jovsatools/minitorch/Module-1/minitorch/modules.py
@@ -1,60 +1,139 @@
-# from .tensor import rand
-# from .functions import matmul, conv2d
-# from .module import Module, Parameter
+## Task 0.4
+## Modules
 
 
-# class tLinear(Module):
-#     def __init__(self, in_size, out_size):
-#         super().__init__()
-#         self.weights = Parameter(rand((in_size, out_size)))
-#         self.bias = Parameter(rand((out_size,)))
-#         self.out_size = out_size
+class Module:
+    """
+    Attributes:
+        _modules (dict of name x :class:`Module`): Storage of the child modules
+        _parameters (dict of name x :class:`Parameter`): Storage of the module's parameters
+        mode (string): Mode of operation, can be {"train", "eval"}.
 
-#     def forward(self, x):
-#         batch, in_size = x.shape
-#         return (
-#             self.weights.value.view(1, in_size, self.out_size)
-#             * x.view(batch, in_size, 1)
-#         ).sum(1).view(batch, self.out_size) + self.bias.value.view(1, self.out_size)
+    """
+
+    def __init__(self):
+        self._modules = {}
+        self._parameters = {}
+        self.mode = "train"
+
+    def modules(self):
+        "Return the child modules of this module."
+        return self.__dict__["_modules"].values()
+
+    def train(self):
+        "Set the mode of this module and all descendent modules to `train`."
+        self.mode = "train"
+        for m in self.modules():
+            m.train()
+
+    def eval(self):
+        "Set the mode of this module and all descendent modules to `eval`."
+        self.mode = "eval"
+        for m in self.modules():
+            m.eval()
+
+    def named_parameters(self):
+        """
+        Collect all the ancestor parameters of this module.
+        Returns:
+            dict: Each name (key) and :class:`Parameter` (value) under this module.
+        """
+        parameters = {}
+        for k, v in self._parameters.items():
+            parameters[k] = v
+
+        for mod_name, m in self._modules.items():
+            for k, v in m.named_parameters().items():
+                parameters[f"{mod_name}.{k}"] = v
+        return parameters
+
+    def parameters(self):
+        return self.named_parameters().values()
+
+    def add_parameter(self, k, v):
+        """
+        Manually add a parameter. Useful helper for scalar parameters.
+
+        Args:
+            k (str): Local name of the parameter.
+            v (value): Value for the parameter.
+
+        Returns:
+            Parameter: Newly created parameter.
+        """
+        val = Parameter(v)
+        self.__dict__["_parameters"][k] = val
+        return val
+
+    def __setattr__(self, key, val):
+        if isinstance(val, Parameter):
+            self.__dict__["_parameters"][key] = val
+        elif isinstance(val, Module):
+            self.__dict__["_modules"][key] = val
+        else:
+            super().__setattr__(key, val)
+
+    def __getattr__(self, key):
+        if key in self.__dict__["_parameters"]:
+            return self.__dict__["_parameters"][key]
+
+        if key in self.__dict__["_modules"]:
+            return self.__dict__["_modules"][key]
+
+        return self.__getattribute__(key)
+
+    def __call__(self, *args, **kwargs):
+        return self.forward(*args, **kwargs)
+
+    def forward(self):
+        assert False, "Not Implemented"
+
+    def __repr__(self):
+        def _addindent(s_, numSpaces):
+            s = s_.split("\n")
+            if len(s) == 1:
+                return s_
+            first = s.pop(0)
+            s = [(numSpaces * " ") + line for line in s]
+            s = "\n".join(s)
+            s = first + "\n" + s
+            return s
+
+        child_lines = []
+
+        for key, module in self._modules.items():
+            mod_str = repr(module)
+            mod_str = _addindent(mod_str, 2)
+            child_lines.append("(" + key + "): " + mod_str)
+        lines = child_lines
+
+        main_str = self.__class__.__name__ + "("
+        if lines:
+            # simple one-liner info, which most builtin Modules will use
+            main_str += "\n  " + "\n  ".join(lines) + "\n"
+
+        main_str += ")"
+        return main_str
 
 
-# class tLinear2(Module):
-#     def __init__(self, in_size, out_size):
-#         super().__init__()
-#         self.weights = Parameter(rand((in_size, out_size)))
-#         self.bias = Parameter(rand((out_size,)))
-#         self.out_size = out_size
+class Parameter:
+    """
+    A Parameter is a special container stored in a :class:`Module`.
 
-#     def forward(self, x):
-#         batch, in_size = x.shape
-#         return matmul(x, self.weights.value) + self.bias.value.view(1, self.out_size)
+    It is designed to hold a :class:`Variable`, but we all it to hold
+    any value for testing.
+    """
 
+    def __init__(self, x=None):
+        self.value = x
+        if hasattr(x, "requires_grad_"):
+            self.value.requires_grad_(True)
 
-# class Dropout(Module):
-#     def __init__(self, rate):
-#         super().__init__()
-#         self.rate = rate
+    def update(self, x):
+        "Update the parameter value."
+        self.value = x
+        if hasattr(x, "requires_grad_"):
+            self.value.requires_grad_(True)
 
-#     def forward(self, x):
-#         return (rand(x.shape) / 2 + 0.5 < self.rate) * x
-
-
-# class Conv2d(Module):
-#     def __init__(self, in_features, out_features, size):
-#         super().__init__()
-#         size1 = [size[0], size[1], in_features, out_features]
-#         size2 = [size[0], size[1], out_features]
-#         self.weights = Parameter(rand(size1))
-#         self.bias = Parameter(rand(size2))
-
-#     def forward(self, x):
-#         return conv2d(x, self.weights.value, self.bias.value)
-
-
-# # class MaxPool2d(Module):
-# #     def __init__(self, in_features, out_features, size):
-# #         super().__init__()
-
-
-# #     def forward(self, x):
-# #         return conv2d(x, self.weights.value, self.bias.value)
+    def __repr__(self):
+        return repr(self.value)
